6e2ea0c092bddbdcdc54a60c1e16bc19f1503b1b
Alex Younger <alex@izoox.com>
2020-11-21 01:52:22 -0700

    Testing terrain generator:

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,23 @@ const del = require('del');
 const gulpSequence = require('gulp-sequence')
 const livereload = require('gulp-livereload');
 
-gulp.task('icons', () => {
-	return gulp
-		.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
-		.pipe(gulp.dest('public/fonts/'))
-});
+// gulp.task('icons', () => {
+// 	return gulp
+// 		.src('node_modules/@fortawesome/fontawesome-free/webfonts/*')
+// 		.pipe(gulp.dest('public/fonts/'))
+// });
 
 gulp.task('styles', () => {
 	return gulp
 		.src('scss/app.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(gulp.dest('public/css/'));
+});
+
+gulp.task('pages', () => {
+	return gulp
+		.src('scss/pages/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer())
 		.pipe(gulp.dest('public/css/'));
@@ -31,4 +39,4 @@ gulp.task('clean', () => {
 	]);
 });
 
-gulp.task('default', gulpSequence('clean', 'icons', 'styles'));
+gulp.task('default', gulpSequence('clean', 'styles', 'pages'));
